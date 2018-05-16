1361243e02b2774ef70429492a09b0c87b71cd3a
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-05-16 08:50:43 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,88 @@
+module.exports = {
+
+    cursusList: [
+        {
+            name: 'Spring',
+            trainings: [
+                {
+                    name: 'Spring Context',
+                    description: `
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-spring-context'
+                },
+                {
+                    name: 'Spring Data',
+                    description: `
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-spring-data'
+                },
+                {
+                    name: 'Spring Web MVC',
+                    description: `
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-spring-webmvc'
+                },
+                {
+                    name: 'Spring Security',
+                    description: `
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-spring-security'
+                },
+                {
+                    name: 'Spring Boot',
+                    description:`
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-spring-boot'
+                }
+            ]
+        },
+        {
+            name: 'Front-End',
+            description: `
+                Cursus de formation Angular.
+            `,
+            trainings: [
+                {
+                    name: 'Introduction à NPM',
+                    description: `
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-intro-node'
+                },
+                {
+                    name: 'ECMAScript 2015+',
+                    description:`
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-esnext'
+                },
+                {
+                    name: 'TypeScript',
+                    description: `
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-typescript'
+                },
+                {
+                    name: 'Webpack',
+                    description: `
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-webpack'
+                },
+                {
+                    name: 'Angular',
+                    description: `
+                       Lorem ipsum dolor sit amet consectetur adipiscing elit blandit quisque elementum, primis viverra scelerisque dictumst cubilia sollicitudin cursus porta mauris aliquet, ullamcorper torquent penatibus pulvinar molestie luctus ad volutpat himenaeos. Magnis maecenas cum leo faucibus ad aptent nam, primis aliquet dapibus sed et neque, iaculis montes viverra malesuada eu dictumst. Ultricies tellus phasellus habitasse imperdiet tortor nec cras, senectus vestibulum nunc dui netus platea augue, turpis leo pellentesque suscipit posuere condimentum.
+                    `,
+                    git: 'https://github.com/DevInstitut/formation-angular'
+                }
+            ]
+        }
+    ]
+};
